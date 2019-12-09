ec4ca83ac3c4a827558138eb50f9b54a210f5d08
samuel950523 <samuel95@naver.com>
2019-12-09 15:20:15 +0900

    1

--- psy/function.py
+++ psy/function.py
@@ -1,6 +1,6 @@
 #----------------------------------#
-# 반드시 home dir에서 실행시키시오   
-# 또는 file open의 dir을 변경하시오  
+# 반드시 home dir에서 실행시키시오
+# 또는 file open의 dir을 변경하시오
 #----------------------------------#
 
 
@@ -17,7 +17,8 @@ def q(f):
         # 답을 하는 line #
         elif i % 2 == 1:
             print("답 : ", end='')
-            if(input()=='0'): break
+            if input() == '0':
+                break
             print("답 : " + line, end='')
 
         # 끝나면 break #
@@ -26,9 +27,26 @@ def q(f):
         i += 1
 
 
-# 여기에 text file의 이름을 적으시오 #
-# "file_name/file_name/file_name.txt" #
-f = open("train.txt", 'r', encoding='UTF8')
+print("1. bio")
+print("2. sensation_and_perception")
+print("3. learning")
+print("4. memory")
+print("5. emotion")
+print("6. train")
+
+a = int(input())
+if a == 1:
+    f = open("1.bio.txt", 'r', encoding='UTF8')
+elif a == 2:
+    f = open("2.sensation_and_perception.txt", 'r', encoding='UTF8')
+elif a == 3:
+    f = open("3.learning.txt", 'r', encoding='UTF8')
+elif a == 4:
+    f = open("4.memory.txt", 'r', encoding='UTF8')
+elif a == 5:
+    f = open("5.emotion.txt", 'r', encoding='UTF8')
+elif a == 6:
+    f = open("train.txt", 'r', encoding='UTF8')
 q(f)
 # close #
 f.close()
